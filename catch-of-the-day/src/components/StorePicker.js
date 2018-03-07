b333e0759145a9507bc36dd17c803df2008c3ad4
Kay Krewson <kay.k@comcast.net>
2018-03-07 03:58:50 -0800

    form submission

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,13 +1,28 @@
 import React, {Component} from 'react';
 import { render } from 'react-dom';
+import { getFunName } from '../helpers';
 
 class StorePicker extends Component {
+  myInput = React.createRef();
+
+  goToStore = event => {
+    event.preventDefault();
+    const storeName = this.myInput.value.value;
+    this.props.history.push(`/store/${storeName}`);
+  }
+
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
       <h2>Please Enter a Store</h2>
-      <input type="text" required placeholder="Store Name" />
-      <button type="submit">Visit</button>
+      <input
+        type="text"
+        ref={this.myInput}
+        required
+        placeholder="Store Name"
+        defaultValue={getFunName()}
+      />
+      <button type="submit">Visit Store</button>
       </form>
     )
   }
